c72eb03aa3f8e708210ee4e11a77dab830778033
Samuel Novotný <samuel.novotn@gmail.com>
2024-11-21 00:42:00 +0100

    ac 2, buttons

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.getElementById("background-video");
-  const muteButton = document.getElementById("snd-button");
-  const muteIcon = document.getElementById("mute-icon");
+  const muteIcons = {
+    button1: document.getElementById("mute-icon"),
+    button2: document.getElementById("mute2"),
+  };
+  const muteButtons = document.querySelectorAll("#snd-button, #snd-button2");
 
-  muteButton.addEventListener("click", () => {
-    if (video.muted) {
-      video.muted = false;
-      muteIcon.src = "/img/sound-on.png";
-      muteIcon.alt = "Sound On";
-    } else {
-      video.muted = true;
-      muteIcon.src = "/img/sound-off.png";
-      muteIcon.alt = "Sound Off";
-    }
+  muteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (video.muted) {
+        video.muted = false;
+        if (muteIcons.button1) {
+          muteIcons.button1.src = "/img/sound-on.png";
+          muteIcons.button1.alt = "Sound On";
+        }
+        if (muteIcons.button2) {
+          muteIcons.button2.src = "/img/sound-on-black.png";
+          muteIcons.button2.alt = "Sound On";
+        }
+      } else {
+        video.muted = true;
+        if (muteIcons.button1) {
+          muteIcons.button1.src = "/img/sound-off.png";
+          muteIcons.button1.alt = "Sound Off";
+        }
+        if (muteIcons.button2) {
+          muteIcons.button2.src = "/img/sound-off-black.png";
+          muteIcons.button2.alt = "Sound Off";
+        }
+      }
+    });
   });
 });
